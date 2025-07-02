64cf2f1539a801c4e720d4607ce4a6c7c1df6cd8
neutrino2211 <neutrino2211@gmail.com>
2025-07-02 23:26:04 +0100

    Add hijri calendar convertion function

--- src/components/blog-post.tsx
+++ src/components/blog-post.tsx
@@ -1,13 +1,15 @@
 import type { PropsWithChildren } from "react";
 import type { BlogMetadata } from "@/types/blog";
+import { getHijriDate } from "@/lib/utils";
 
-export default function BlogPost({meta, children}: PropsWithChildren<{meta: BlogMetadata}>) {
+export default async function BlogPost({meta, children}: PropsWithChildren<{meta: BlogMetadata}>) {
+    const date = await getHijriDate(new Date(meta.publishedAt))
     return (
         <div>
             <h1 className="text-6xl font-light pb-8">{meta.title}</h1>
             <div className="flex justify-between items-center my-4 dark:text-[#444] text-[#aaa]">
                 <p className="font-bold">FROM {" "}<span className="text-foreground font-light">{meta.author}</span></p>
-                <p className="font-bold">DATE {" "} <span className="text-foreground font-light">{new Date(meta.publishedAt).toLocaleDateString()}</span></p>
+                <p className="font-bold">DATE {" "} <span className="text-foreground font-light">{date.date}</span></p>
             </div>
             <hr />
             <div className="pt-4 text-xl">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,51 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export interface Hijri {
+  date: string
+  format: string
+  day: string
+  weekday: Weekday
+  month: Month
+  year: string
+  designation: Designation
+  holidays: any[]
+  adjustedHolidays: any[]
+  method: string
+}
+
+export interface Weekday {
+  en: string
+  ar: string
+}
+
+export interface Month {
+  number: number
+  en: string
+  ar: string
+  days: number
+}
+
+export interface Designation {
+  abbreviated: string
+  expanded: string
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export async function getHijriDate(date: Date) {
+  const day = date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  const apiUrl = `https://api.aladhan.com/v1/gToH/${day.split("/").join("-")}?calendarMethod=HJCoSA`;
+
+  const response = await fetch(apiUrl); 
+
+  const data = await response.json();
+  return data.data.hijri as Hijri;
+}
